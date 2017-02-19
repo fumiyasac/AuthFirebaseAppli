a69fbe232a557af512ed51ab1d29ba50435e1fa0
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-02-19 23:57:35 +0900

    install firebase and initialize

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Header } from './components/common';
 
+//firebaseのインポート宣言を行う
+import firebase from 'firebase';
+
 class App extends Component {
+
+  //コンポーネントの内容がMountされる前に行う処理
+  componentWillMount() {
+
+    //firebaseのセッティング情報を記載する
+    firebase.initializeApp({
+      apiKey: "AIzaSyDVwEbIjIT66gvdHFPhSvD6nekm3d6w0l8",
+      authDomain: "authfirebaseappli.firebaseapp.com",
+      databaseURL: "https://authfirebaseappli.firebaseio.com",
+      storageBucket: "authfirebaseappli.appspot.com",
+      messagingSenderId: "919888847951"
+    });
+  }
+
+  //見た目データのレンダリングを行う
   render() {
     return (
       <View>
@@ -15,4 +33,5 @@ class App extends Component {
   }
 }
 
+//アプリの画面本体となるこのコンポーネントファイルを部品化しておく
 export default App;
